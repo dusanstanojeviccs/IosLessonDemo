692c1bc0af54892132b0e1b7ca67e16992b3730c
dusanstanojeviccs <dusan.stanojevic.cs@gmail.com>
2016-06-09 21:48:56 +0200

    Git init

---
+++ Demo/Demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Demo
+//
+//  Created by Dusan Stanojevic on 6/9/16.
+//  Copyright © 2016 Dusan Stanojevic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
